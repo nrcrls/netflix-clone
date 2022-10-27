ea37eb3d41d91ebab19aca0e752c2d9490566f19
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-10-27 22:32:59 +0900

    fixed slider styles

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -17,7 +17,7 @@ function Row({ title, fetchURL }) {
     fetchData();
   }, [fetchURL]);
 
-  console.log(movies);
+
 
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
@@ -27,10 +27,11 @@ function Row({ title, fetchURL }) {
 
       {/* slider buttons */}
       <div className="group relative md:-ml-2">
-        <ChevronLeftIcon className="absolute top-0 bott0m-0 left-2 z-40 m-auto w-9 h-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronLeftIcon className="absolute top-0 bottom-0 left-0 z-40 m-auto w-[50px] h-full cursor-pointer opacity-0 group-hover:opacity-100 group-hover:bg-[#161616] group-hover:bg-opacity-75"
+        />
 
         {/* movie thumbnails */}
-        <div className="flex bg-[#141414] relative space-x-0.5 overflow-y-hidden overflow-x-scroll scrollbar-hide md:space-x-4 md:p-2">
+        <div className="flex relative space-x-0.5 overflow-y-hidden overflow-x-scroll scrollbar-hide md:space-x-4 md:p-2">
           {movies.map((movie) =>
             <img
               className='cursor-pointer object-cover rounded h-28 min-w-[180px] transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'
@@ -41,7 +42,8 @@ function Row({ title, fetchURL }) {
           )}
         </div>
 
-        <ChevronRightIcon className="absolute top-0 bott0m-0 left-2 z-40 m-auto w-9 h-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronRightIcon className="absolute top-0 bottom-0 right-0 z-40 m-auto w-[52px] h-full  cursor-pointer opacity-0 group-hover:opacity-100 group-hover:bg-[#161616] group-hover:bg-opacity-75"
+        />
       </div>
     </div>
   );

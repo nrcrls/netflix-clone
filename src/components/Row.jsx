23854073b06ad69e17135cc38bd5931426fc4381
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-10-28 14:49:07 +0900

    fixed some styling

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -4,8 +4,8 @@ import { useEffect, useRef, useState } from "react";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
-  const listRef = useRef()
-  const [isMoved, setIsMoved] = useState(false)
+  const listRef = useRef();
+  const [isMoved, setIsMoved] = useState(false);
 
   const base_url = 'https://image.tmdb.org/t/p/original/'
 
@@ -21,6 +21,7 @@ function Row({ title, fetchURL }) {
 
   const handleClick = (direction) => {
     setIsMoved(true)
+
     if (listRef.current) {
       const { scrollLeft, clientWidth } = listRef.current
 
@@ -36,7 +37,7 @@ function Row({ title, fetchURL }) {
     <div className="h-40 space-y-0.5 md:space-y-2">
 
       {/* title */}
-      <h2 className="cursor-pointer md:mt-28 md:pt-24 w-56 text-sm md:text-3xl font-semibold text-[#e5e5e5] transition duration-200 hover:text-white">{title}</h2>
+      <h2 className="cursor-pointer md:mt-16 md:pt-24 w-56 text-sm md:text-2xl font-semibold text-[#e5e5e5] transition duration-200 hover:text-white">{title}</h2>
 
       {/* slider buttons */}
       <div className="group relative md:-ml-2">
@@ -46,7 +47,9 @@ function Row({ title, fetchURL }) {
         />
 
         {/* movie thumbnails */}
-        <div className="flex relative space-x-0.5 overflow-y-hidden overflow-x-scroll scrollbar-hide md:space-x-4 md:p-2 transition-all duration-100 ease" ref={listRef}>
+        <div className="flex relative space-x-0.5 overflow-y-hidden overflow-x-scroll scrollbar-hide md:space-x-4 md:p-2 transition-all duration-100 ease"
+          ref={listRef}
+        >
           {movies.map((movie) =>
             <img
               className='cursor-pointer object-cover rounded h-28 min-w-[180px] transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'
